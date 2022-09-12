15a0e36ba58374d71f817f9c75ab1c6da7dcf6c2
Gabriel Mariz <gabriellucena997@outlook.com>
2022-09-12 19:35:51 -0300

    :sparkles: feat: global chat input model

---
+++ Domain/Models/GlobalChatInput.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models;
+
+public class GlobalChatInput
+{
+	[Required(ErrorMessage = "Message required")]
+	[MaxLength(160, ErrorMessage = "160 is the maximum number of characters allowed.")]
+	public string? Message { get; set; }
+
+	[Required(ErrorMessage = "User id required")]
+	public Guid UserId { get; set; }
+
+	public Guid FromId { get; set; }
+}
