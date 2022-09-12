d929eed1af996d91414d7d352a4a5fd3371f0a4b
Gabriel Mariz <gabriellucena997@outlook.com>
2022-09-12 19:37:01 -0300

    :sparkles: feat: update migration new global chat

---
+++ Infra/Migrations/20220912223109_GlobalChatMigration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    public partial class GlobalChatMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GlobalChats",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Message = table.Column<string>(type: "varchar(160)", maxLength: 160, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    From = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    To = table.Column<Guid>(type: "char(36)", nullable: true, collation: "ascii_general_ci"),
+                    CreateAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GlobalChats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GlobalChats_Profiles_From",
+                        column: x => x.From,
+                        principalTable: "Profiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GlobalChats_From",
+                table: "GlobalChats",
+                column: "From",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GlobalChats");
+        }
+    }
+}
