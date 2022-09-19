0360939b29672b47946a2a51e31e784f69333da1
Gabriel Mariz <gabriellucena997@outlook.com>
2022-09-19 19:58:17 -0300

    :sparkles: feat: components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/fragments/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FooterComponent } from './components/fragments/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RegisterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
